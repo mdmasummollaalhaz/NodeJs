fd06c50027f1c804d3d416ba36f7859390315313
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-20 13:29:28 +0600

    Init

---
+++ __steps.js
@@ -0,0 +1,11 @@
+/**
+ * 1. create a folder ( manually or mkdir)
+ * 2. open command line to that folder
+ * 3. npm init
+ * 4. npm install express
+ * 5. Create index.js
+ * ---------------------------
+ * For Express/Node server
+ * ---------------------------
+ * 1. 
+ */
